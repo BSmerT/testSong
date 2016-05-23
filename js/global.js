e2b69231a43d379b3777f5613ca3968a38444bfe
fabian_zurita <zurita.salgado.fabian@gmail.com>
2016-05-22 23:53:16 -0300

    Se agrega autentificación al ingresar al home y conecta con el usuario para poder acceder a las listas de spotify y para poder reproducir las canciones, pero aún asi se necesita de la aplicación para poder escuchar via web :/

--- js/global.js
+++ js/global.js
@@ -1,8 +1,11 @@
 var datag = [];
+var dataTop = [];
+var tokenGet = "";
 var spotifyApi = new SpotifyWebApi();
-spotifyApi.setAccessToken('BQAYBHE8kDFxrP1r_45ybAV9W4wNlxNqOMLLr1-Kr4-bVmDNLL7W4jOX4ZCDrmWdqNEENstL8dZk2KNYVF2YF3vjlO594N2U5Aib_2xuqTCMwT7oi6HOHEqa6nfFJWChuIHxww');
 
 $(document).ready(function() {
+    valdiateLogin(localStorage.getItem('tokenGet'));
+    
     $('#search').on('input', function() {
         var value = $(this).val();
         if (value.length >= 3) {
@@ -18,8 +21,35 @@ $(document).ready(function() {
         showAndPlay(datag.tracks.items[$(this).data('value')]);    
     });
     
-    getListTop();
+    $('.top').on('mouseenter', 'li',function() {
+        value = $(this).data('value');
+        url = dataTop.items[value].track.preview_url;
+        if (url) {
+            console.log('le carog');
+        }else{
+            console.log('no cargo');
+        }
+        
+        audioObject = new Audio(url);
+        audioObject.play();
+    }).on('mouseleave', 'li',function() {
+        audioObject.pause();
+    });
+    
+    $('#btn-login').on('click', function(){
+        login();
+    });
 });
+
+function valdiateLogin(validateToken) {
+    if (!validateToken) {
+        $('.box-login').removeClass('disabled');
+    }else{
+        spotifyApi.setAccessToken(validateToken);
+        dataTop = getListTop();
+        $('.box-login').addClass('disabled');
+    }
+}
 
 function searchSong(value) {
     spotifyApi.searchTracks(value, {limit: 5})
@@ -42,14 +72,49 @@ function getListTop() {
     spotifyApi.getPlaylistTracks('spotifyenchile','2Q3ZdOPitcusSmSXhmdIDB',{limit: 10})
     .then(function(data) {
         console.log(data);
+        dataTop = data;
         $.each(data.items, function(i, item) {
             liSong(data.items[i].track,i, "top");
         });
+    }, function(err) {
+        //console.error(err);
     });
+    return dataTop;
 }
 function clearDiv(div) {
     $("."+div).html("");
 }
 function liSong(datos,i,div) {
     $("."+div).append("<li data-value='"+i+"'><img alt='Album' src='"+datos.album.images[1].url+"'><h4>"+datos.name+"</h4><b>By "+datos.artists[0].name+" </b><em>Albúm "+datos.album.name+"</em></li>");
+}
+
+function login(callback) {
+    var CLIENT_ID = '8d1f133be7644028b5835d5c7b450360';
+    var REDIRECT_URI = 'http://localhost/test/callback';
+    function getLoginURL(scopes) {
+        return 'https://accounts.spotify.com/authorize?client_id=' + CLIENT_ID +
+          '&redirect_uri=' + encodeURIComponent(REDIRECT_URI) +
+          '&scope=' + encodeURIComponent(scopes.join(' ')) +
+          '&response_type=token'; //Using client credentials flow
+    }
+    var url = getLoginURL([
+        '' // no scopes required
+    ]);
+    var width = 450,
+        height = 730,
+        left = (screen.width / 2) - (width / 2),
+        top = (screen.height / 2) - (height / 2);
+    
+    // listen for callback
+    window.addEventListener("message", function(event) {
+        var hash = JSON.parse(event.data);
+        if (hash.type == 'access_token') {
+            console.log(hash.access_token);
+            localStorage.setItem('tokenGet', hash.access_token);
+            lala = localStorage.getItem('tokenGet');
+            valdiateLogin(lala);
+        }
+    }, false);
+    
+    var w = window.open(url,'Spotify','menubar=no,location=no,resizable=no,scrollbars=no,status=no, width=' + width + ', height=' + height + ', top=' + top + ', left=' + left);
 }
